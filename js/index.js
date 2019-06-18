d2923365bce9b24b62b02f380c91a207ab9b1ab2
Areum Jo <areumjo1@gmail.com>
2019-06-18 14:59:34 -0400

    Initial commit - project set up

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+/* 
+10 unique event listeners
+ mouseover
+ keydown
+ wheel
+ drag / drop
+ load
+ focus : fires when an element has received focus. 
+The main difference between this event and focusin is that focusin bubbles while focus does not.
+The opposite of focus is blur.
+document.addEventListener('focus',function(e){ //some code }, true);
+ resize
+ scroll
+ select
+ dblclick
+ */
